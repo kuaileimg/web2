18e379a65f7c551f5356d0f606bb31ea8f0673ad
kjw <1186751305@qq.com>
2019-08-01 10:03:39 +0800

    Initial commit

---
+++ p1.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>p1</title>
+</head>
+<body>
+<h1>php wang ye </h1>
+  <?php
+    echo "hello php <br>";//输出
+    $x=5;
+    $y=8;
+    $z=$x+$y;
+    echo $z;
+  ?>
+</body>
+</html>
